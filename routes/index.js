3557588f78b12b1ee7af9c5a2da184c40239a959
张恒 <zhfirefly@yahoo.com>
2018-09-04 22:23:06 +0800

    创建数据库表

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 'use strict';
 import admin from './admin.js';
 import createData from './createData.js';
+import customer from './customer.js';
 export default app=>{
     app.use('/call/admin',admin);
     app.use('/call/createvirtualdata',createData);
+    app.use('/call/customer',customer);
 }
